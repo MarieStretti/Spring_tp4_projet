ba75dd516d333fe8b102b7041943b687bddee70d
MarieStr <mariestretti@gmail.com>
2020-02-03 11:30:57 +0100

    Initialize git repo

---
+++ src/main/java/eu/ensg/Spring_tp4_projet/SpringTp4ProjetApplication.java
@@ -0,0 +1,13 @@
+package eu.ensg.Spring_tp4_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTp4ProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTp4ProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/ensg/Spring_tp4_projet/SpringTp4ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensg.Spring_tp4_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTp4ProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
