f24bc92e412f2e3e70006622174f31fe94d0fd59
MarieStr <mariestretti@gmail.com>
2020-02-05 21:56:09 +0100

    Form try

--- src/main/java/eu/ensg/Spring_tp4_projet/controller/FormulaireParticipantController.java
+++ src/main/java/eu/ensg/Spring_tp4_projet/controller/FormulaireParticipantController.java
@@ -1,11 +1,23 @@
 package eu.ensg.Spring_tp4_projet.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import eu.ensg.Spring_tp4_projet.model.Participant;
 import eu.ensg.Spring_tp4_projet.model.ParticipantRepository;
@@ -31,4 +43,29 @@ public class FormulaireParticipantController {
         participantRepository.save(p);
         return "addParticipant";
     }
+    
+//    @GetMapping("/add")
+//    public String addNewParticipant(Model model) {
+//        model.addAttribute("participantForm", new Participant());
+//        return "addNewParticipant";
+//    }
+//    
+//    @PostMapping("/add")
+//    public String submitParticipant(@ModelAttribute("participantForm") Participant p) {
+//        participantRepository.save(p);
+//        return "result";
+//    }
+
+//    @GetMapping("/")
+//    public String greetingForm(Model model) {
+//      model.addAttribute("", new Participant());
+//      return "index";
+//    }
+//
+//    @PostMapping("/")
+//    public String greetingSubmit(@ModelAttribute Participant p) {
+//      return "result";
+//    }
+    
+    
 }
